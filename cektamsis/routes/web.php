5c64b9a2ad514b5e8837fbd667d6431ab4e48487
bruryansah <bruryansah@gmail.com>
2025-08-16 10:57:35 +0700

    The Zeroth

---
+++ cektamsis/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/tentang', function () {
+    return Inertia::render('Tentang');
+});
+Route::get('/fitur', function () {
+    return Inertia::render('Fitur');
+});
+Route::get('/kontak', function () {
+    return Inertia::render('Kontak');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
